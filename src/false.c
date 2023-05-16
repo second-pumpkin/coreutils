cd81db651b5a1943a5f0d3de13cf151ec9873b56
Logan Goins <55106700+Lionskey@users.noreply.github.com>
2023-05-16 18:12:18 -0500

    Update false.c

--- src/false.c
+++ src/false.c
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------ 
-A less bloated version of GNU Frue - Do nothing, unsuccessfully.
+A less bloated version of GNU False - Do nothing, unsuccessfully.
 Like, come'on GNU, this is all it had to be :\
 (GNU devs really don't know how to return 1 smh)
 Author: Logan Goins
